a9aed0ed82203cc2be3f73c410aca8a7b554eee1
daoninhthai <thaimeo1131@gmail.com>
2020-10-13 11:29:54 +0700

    xoa file thua

--- BaiTapThucHanh/Car5/src/main/java/com/example/Car1/config/RepoConfig.java
+++ BaiTapThucHanh/Car5/src/main/java/com/example/Car1/config/RepoConfig.java
@@ -22,4 +22,16 @@ public class RepoConfig {
             .format(java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
     }
 
+
+    /**
+     * Validates that the given value is within the expected range.
+     * @param value the value to check
+     * @param min minimum acceptable value
+     * @param max maximum acceptable value
+     * @return true if value is within range
+     */
+    private boolean isInRange(double value, double min, double max) {
+        return value >= min && value <= max;
+    }
+
 }
