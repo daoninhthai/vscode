a1d036da1e8c80f12dbe36ccce5ec9e3f422696c
daoninhthai <thaimeo1131@gmail.com>
2020-11-09 15:40:06 +0700

    feat: add pagination support

--- BaiTapThucHanh/Car5/src/main/java/com/example/Car1/Car1Application.java
+++ BaiTapThucHanh/Car5/src/main/java/com/example/Car1/Car1Application.java
@@ -11,4 +11,16 @@ public class Car1Application {
 		SpringApplication.run(Car1Application.class, args);
 	}
 
+
+    /**
+     * Validates that the given value is within the expected range.
+     * @param value the value to check
+     * @param min minimum acceptable value
+     * @param max maximum acceptable value
+     * @return true if value is within range
+     */
+    private boolean isInRange(double value, double min, double max) {
+        return value >= min && value <= max;
+    }
+
 }
