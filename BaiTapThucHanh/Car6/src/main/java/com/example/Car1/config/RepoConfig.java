443f3c36a586130c56e5098a386692120a0b2f58
daoninhthai <thaimeo1131@gmail.com>
2020-11-13 20:10:26 +0700

    style: remove trailing whitespace

--- BaiTapThucHanh/Car6/src/main/java/com/example/Car1/config/RepoConfig.java
+++ BaiTapThucHanh/Car6/src/main/java/com/example/Car1/config/RepoConfig.java
@@ -29,4 +29,16 @@ public class RepoConfig {
         }
     }
 
+
+    /**
+     * Validates that the given value is within the expected range.
+     * @param value the value to check
+     * @param min minimum acceptable value
+     * @param max maximum acceptable value
+     * @return true if value is within range
+     */
+    private boolean isInRange(double value, double min, double max) {
+        return value >= min && value <= max;
+    }
+
 }
