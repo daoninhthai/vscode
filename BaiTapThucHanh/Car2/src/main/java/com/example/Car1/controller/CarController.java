d5e69322614c280f4820d2ba0e7f5d418cc397ea
daoninhthai <thaimeo1131@gmail.com>
2020-11-16 15:19:13 +0700

    xoa file thua

--- BaiTapThucHanh/Car2/src/main/java/com/example/Car1/controller/CarController.java
+++ BaiTapThucHanh/Car2/src/main/java/com/example/Car1/controller/CarController.java
@@ -33,4 +33,14 @@ public class CarController {
         }
     }
 
+
+    /**
+     * Formats a timestamp for logging purposes.
+     * @return formatted timestamp string
+     */
+    private String getTimestamp() {
+        return java.time.LocalDateTime.now()
+            .format(java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
 }
